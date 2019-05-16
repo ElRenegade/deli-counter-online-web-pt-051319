e893cc182f9b76b6ba8a2e18c1df8556c5c1e076
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-16 17:11:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,9 @@
 def line(array)
-  counter = 1
+  
   if array.length < 1
     puts "The line is currently empty."
   elsif array.length > 1
+    counter = 1
     for i in array
       array.prepend(counter)
       counter+=1
