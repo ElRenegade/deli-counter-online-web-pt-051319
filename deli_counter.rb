67f3f616178a31a22a84f697a528caad08e35561
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-16 17:07:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,10 @@ def line(array)
   if array.length < 1
     puts "The line is currently empty."
   elsif array.length > 1
-    for i in array
+    for i in array.length
       array.prepend(counter)
       counter+=1
-      cline = array.join(".")
+      cline = array.join(" .")
       puts "The line is currently: #{cline} "
     end
   end
