5322cbe0daed32fa6d8820c39680e7cd1e0ba484
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-16 17:37:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,21 @@
 def line(array)
-
   if array.length < 1
     puts "The line is currently empty."
   elsif array.length > 1
     num = 1
-    # array.each{ |customer| customer.prepend(" #{num}. ")}
-    # line = array.join
     for i in array
       i.prepend(" #{num}. ")
       num+=1
     end
     line = array.join
     puts "The line is currently:#{line}"
+  end
+end
+
+def take_a_number(array, name)
+  couner = 1
+  for i in array
+    puts "Welcom #{name} you are number #{counter} in line."
+    counter+=1
   end
 end
