9d09c405a97a31f15dcddec0d70b3335b0df3b25
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-16 17:21:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
     puts "The line is currently empty."
   elsif array.length > 1
     counter= 1
-    array.each{ |customer| array.prepend("#{counter}.") counter+=1 }
+    array.each{ |customer| array.prepend("#{counter}.")}
     line = array.join
     puts "The line is currently:#{line} "
   end
