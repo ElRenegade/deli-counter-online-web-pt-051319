022fcb824afe75771c4aa6a81d67ae9242e17ba4
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-16 17:00:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ def line(array)
     puts "The line is currently empty."
   elsif array.length > 1
     for i in array
-      puts "The line is currently: #{counter}.#{i}"
-      counter +=1
+      array.append(Counter+".")
+      array.join
     end
   end
 end
